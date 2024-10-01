f4142c0f84fd4c2eb7c88e73e7946b4e6ac1ffcd
sreedsp <160694467+sreedsp@users.noreply.github.com>
2024-10-01 20:25:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+
+from flask import Flask 
+
+def create_app():
+    app = Flask(__name__)
+    @app.route('/')
+    def home():
+        return 'Hi Devops Geeks Welcome to the class123 New Data 123456 New PR'
+
+    return app
+
+    
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=80, debug=True)
