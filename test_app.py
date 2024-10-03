778e7d5a7c264b5522b2f078e2d736b548f32c12
sreedsp <160694467+sreedsp@users.noreply.github.com>
2024-10-03 07:32:53 +0530

    Update test_app.py
    Return value is updated

--- test_app.py
+++ test_app.py
@@ -15,6 +15,6 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'This is a new version'
+    expected_text = 'The changes are made at 7:32'
     print(response.data)
     assert expected_text.encode() == response.data
