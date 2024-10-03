282bcbbccf326ef65a1179936ea72dfa89354679
sreedsp <160694467+sreedsp@users.noreply.github.com>
2024-10-03 07:18:59 +0530

    Update app.py
    Modified return

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def create_app():
     app = Flask(__name__)
     @app.route('/')
     def home():
-        return 'Hello Chatters - Welcome'
+        return 'This is a new version'
 
     return app
 
